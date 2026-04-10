e703a54908b9c03309fe64c764d46741d67bbe06
xPatryk8 <patrykzuk.pz@gmail.com>
2026-04-10 20:00:09 +0200

    Day 5 part 2

--- 2015/day 5/main.c
+++ 2015/day 5/main.c
@@ -45,6 +45,24 @@ bool checkStrings(char line[lineSize], int len) {
   return true;
 }
 
+bool checkDoubleRepeat(char line[], int len) {
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 2; j < len - 1; j++) {
+      if (line[i] == line[j] && line[i + 1] == line[j + 1])
+        return true;
+    }
+  }
+  return false;
+}
+
+bool checkBetween(char line[lineSize], int len) {
+  for (int i = 0; i < len - 2; i++) {
+    if (line[i] == line[i + 2])
+      return true;
+  }
+  return false;
+}
+
 int main() {
 
   FILE *file = fopen("in.txt", "rb");
@@ -54,11 +72,17 @@ int main() {
 
   while (fgets(line, sizeof(line), file)) {
     int len = strlen(line);
-    if (checkDouble(line, len) && checkStrings(line, len) &&
-        checkVowels(line, len)) {
+    // Part 1
+    // if (checkDouble(line, len) && checkStrings(line, len) &&
+    //     checkVowels(line, len)) {
+    //   niceStrings++;
+    // }
+
+    if (line[0] == '\n')
+      continue;
+    if (checkBetween(line, len) && checkDoubleRepeat(line, len))
       niceStrings++;
-    }
   }
 
-  printf("Nice: %d", niceStrings);
+  printf("Nice: %d\n", niceStrings);
 }
